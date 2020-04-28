a6d97a08f0e1f2d47ba8632c3deb0fc19d3007ea
kwngo <kareem.kwong@gmail.com>
2020-04-28 02:11:14 -0400

    Fix query

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from lexio.accounts import create_team, create_user, assign_member_to_team
 from lexio.accounts.models import User
-from .db import SessionLocal, engine
+from db import SessionLocal, engine
 
 from fastapi import Depends, FastAPI, HTTPException
 from pydantic import BaseModel
 from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.orm import Session
 
 app = FastAPI()
 
@@ -30,8 +31,9 @@ class Registration(BaseModel):
     password_confirmation: str
     team_name: str
 
+from lexio.accounts import TeamCreate, UserCreate
 
-@app.get("/register")
+@app.post("/register")
 def register(registration: Registration, db: Session = Depends(get_db)):
     registration_dict = registration.dict()
     email = registration_dict.get('email')
@@ -42,20 +44,26 @@ def register(registration: Registration, db: Session = Depends(get_db)):
     password_confirmation = registration_dict.get('password_confirmation')
     team_name = registration_dict.get('team_name')
 
-    user_exists = db.session.query(User) \
-        .filter((User.email == email) | (User.username == username)) \
+    user_exists = db.query(User) \
+        .filter(User.email == email) \
         .count() > 0
 
-    if user_exists:
+    username_exists = db.query(User) \
+        .filter(User.username == username) \
+        .count() > 0
+
+
+    if user_exists or username_exists:
         raise HTTPException(status_code=400, detail="User could not be created.")
 
     if password != password_confirmation:
         raise HTTPException(status_code=400, detail="Passwords do not match.")
 
+    # Turn this into transaction
     try:
-        team = create_team(db=db, team={"name": team_name})
-        user = create_user(db=db, user={"email": email, "password": password, "username": username, "first_name": first_name, "last_name": last_name})
-        assign_member_to_team(user_id=user.id, team_id=team.id)
+        team = create_team(db=db, team=TeamCreate(name=team_name))
+        user = create_user(db=db, user=UserCreate(email=email, password=password, username=username, first_name=first_name, last_name=last_name))
+        assign_member_to_team(user.id, team.id, db=db)
     except SQLAlchemyError:
         raise HTTPException(status_code=400, detail="New account creation failed")
 
